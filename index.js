e70f422bbe73d918d5c0a9e0ff84c5771f54ae55
whovivekshukla <vivekshuklahere@gmail.com>
2022-12-07 17:22:02 +0530

    -

---
+++ index.js
@@ -0,0 +1,48 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  let unixTimeStamp, utc;
+  if (new Date(date) == "Invalid Date") {
+    if (new Date(parseInt(date)).getTime() == parseInt(date)) {
+      unixTimeStamp = parseInt(date);
+      utc = new Date(parseInt(date)).toGMTString();
+      return res.json({ unix: unixTimeStamp, utc: utc });
+    }
+  }
+  if (date === undefined) {
+    unixTimeStamp = new Date().getTime();
+    utc = new Date().toGMTString();
+  } else {
+    unixTimeStamp = new Date(date).valueOf();
+    utc = new Date(date).toGMTString();
+  }
+  if (isNaN(unixTimeStamp) && utc === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+  return res.json({ unix: unixTimeStamp, utc: utc });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
